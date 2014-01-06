981818e6369944581a43bd9411bd4c5117dbe6e6
Peter T Bosse II <ptb@ioutime.com>
2014-01-05 19:02:27 -0500

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,22 +1,34 @@
 Time.zone = 'America/New_York'
 
-set :index_file, 'index.html'
+set :css_dir, 'css' if File.directory?('source/css/')
+set :images_dir, 'img' if File.directory?('source/img/')
+set :js_dir, 'js' if File.directory?('source/js/')
 
-set :css_dir, 'css'
-set :images_dir, 'img'
-set :js_dir, 'js'
+set :partials_dir, 'partials' if File.directory?('source/partials/')
+
+ignore 'intros/*' if File.directory?('source/intros/')
+
+set :layout, 'minimum'
+with_layout false do
+  page '*.rss'
+  page '*.xml'
+  page '*.css'
+end
 
 activate :blog do |blog|
   blog.sources = 'content/:title.html'
   blog.default_extension = '.slim'
   blog.summary_separator = /(READMORE)/
 
-  blog.tag_template = 'articles.html'
-  blog.calendar_template = 'articles.html'
+  blog.layout = 'blog'
+  set :layout, 'blog'
 
   blog.permalink = ':title.html'
+
+  blog.tag_template = 'articles.html'
   blog.taglink = ':tag/index.html'
 
+  blog.calendar_template = nil
   blog.year_link = ':year/index.html'
   blog.month_link = ':year/:month/index.html'
   blog.day_link = ':year/:month/:day/index.html'
@@ -26,20 +38,31 @@ activate :blog do |blog|
   blog.page_link = 'page-:num'
 end
 
+I18n.enforce_available_locales = false
+Slim::Engine.set_default_options attr_quote: "'", :format => :xhtml, pretty: true, sort_attrs: true, shortcut: {'@' => {attr: 'role'}, '#' => {attr: 'id'}, '.' => {attr: 'class'}, '%' => {attr: 'itemprop'}, '&' => {tag: 'input', attr: 'type'}}
+set :sass, cache: false, line_comments: false, :style => :expanded
+
 activate :directory_indexes
+# set :index_file, 'index.html'
 
-page '/subscribe/index.atom', :layout => false
+# set :relative_links, true
+# activate :relative_assets
 
-ignore '/intros/'
+activate :asset_hash
 
-Slim::Engine.set_default_options(:attr_wrapper => "'", :format => :xhtml, :pretty => true, :sort_attrs => true)
-::Compass.configuration.sass_options = { :line_comments => false }
-
-#activate :livereload
+# activate :livereload, :host => '0.0.0.0'
 
 configure :build do
-  Slim::Engine.set_default_options(:pretty => false)
-  ::Compass.configuration.sass_options = { :style => :compressed }
+  Slim::Engine.set_default_options pretty: false
 
-  activate :gzip, :exts => %w(.atom .css .html .js .rss .svg .txt .xhtml .xml)
+  set :sass, :style => :compressed
+  # activate :minify_css
+  # compass_config do |config|
+    # config.output_style = :compressed
+  # end
+
+  # activate :minify_javascript
+
+  activate :minify_html
+  activate :gzip, exts: %w(.css .html .js .rss .svg .txt .xhtml .xml .eot .otf .ttf)
 end
